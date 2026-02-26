ec410c63f29e63f305468e23b2b7e589bf8b17aa
Avlis <leo.silva@etu.unicaen.fr>
2026-02-26 15:49:49 +0100

    [ADD] Complétion de la classe: ProduitDAO.java

--- src/main/java/dao/ProduitDAO.java
+++ src/main/java/dao/ProduitDAO.java
@@ -1,4 +1,39 @@
 package dao;
 
+import model.Produit;
+
+import java.sql.*;
+
 public class ProduitDAO {
+
+    private Connection conn;
+
+    public ProduitDAO(Connection conn) {
+        this.conn = conn;
+    }
+
+    public void insert(Produit p) throws SQLException {
+        String sql = "INSERT INTO Produits(nom, prix, quantite) VALUES (?, ?, ?)";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, p.getNom());
+        ps.setDouble(2, p.getPrix());
+        ps.setInt(3, p.getQuantite());
+        ps.executeUpdate();
+    }
+
+    public Produit findByName(String nom) throws SQLException {
+        String sql = "SELECT * FROM Produits WHERE nom = ?";
+        PreparedStatement ps = conn.prepareStatement(sql);
+        ps.setString(1, nom);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            return new Produit(
+                    rs.getString("nom"),
+                    rs.getDouble("prix"),
+                    rs.getInt("quantite")
+            );
+        }
+        return null;
+    }
 }
