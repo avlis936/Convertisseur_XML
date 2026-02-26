443a9d066522261fb4d6cce7ce83c2258ebe024a
Avlis <leo.silva@etu.unicaen.fr>
2026-02-26 15:40:32 +0100

    [ADD] Complétion des classes: Client.java, Produit.java et Commande.java dans le package Model

--- src/main/java/model/Client.java
+++ src/main/java/model/Client.java
@@ -1,4 +1,63 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Client {
+    private int id;
+    private String nom_client;
+    private String email;
+    private String ville;
+
+
+    public Client(String nom_client, String email, String ville) {
+        this.nom_client = nom_client;
+        this.email = email;
+        this.ville = ville;
+    }
+
+    // ID
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Nom client
+    public String getNom_client() {
+        return nom_client;
+    }
+
+    public void setNom_client(String nom_client) {
+        this.nom_client = nom_client;
+    }
+
+    // Email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Ville
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+     @Override
+    public String toString() {
+         return "Client{" +
+                 "id=" + id +
+                 ", nom_client='" + nom_client + '\'' +
+                 ", email='" + email + '\'' +
+                 ", ville='" + ville + '\'' +
+                 '}';
+    }
 }
--- src/main/java/model/Commande.java
+++ src/main/java/model/Commande.java
@@ -1,4 +1,80 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Commande {
+    private int id;
+    private Client client;
+    private Date date;
+    private ArrayList<Produit> produits;
+
+    public Commande(int id, Client client, Date date) {
+        this.id = id;
+        this.client = client;
+        this.date = date;
+        this.produits = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public ArrayList<Produit> getProduits() {
+        return produits;
+    }
+
+    public void setProduits(ArrayList<Produit> produits) {
+        this.produits = produits;
+    }
+
+    public void addProduit(Produit produit) {
+        this.produits.add(produit);
+    }
+
+    public void removeProduit(Produit produit) {
+        this.produits.remove(produit);
+    }
+
+    public void clearProduits() {
+        this.produits.clear();
+    }
+
+    public String showProduits() {
+        String val = "";
+        for (Produit produit : produits) {
+            val += produit.toString() + "\n";
+        }
+        return val;
+    }
+
+    @Override
+    public String toString() {
+        return "Commande{" +
+                "id=" + id +
+                ", client=" + client +
+                ", date=" + date +
+                ", produits{" + showProduits() + '}' +
+                '}';
+    }
 }
